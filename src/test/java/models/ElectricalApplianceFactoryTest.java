21067a3f20634cd11a05d323616292cc7ac57991
Stefaniia Vakulenko <stefany.vak@gmail.com>
2017-08-21 16:24:52 +0300

    Tests update

--- src/test/java/models/ElectricalApplianceFactoryTest.java
+++ src/test/java/models/ElectricalApplianceFactoryTest.java
@@ -1,12 +1,13 @@
 package models;
 
+import exceptions.ApplianceCreationException;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ElectricalApplianceFactoryTest {
 
     @Test
-    public void testCreatesValidObjectFromValidParams() {
+    public void testCreatesValidObjectFromValidParams() throws ApplianceCreationException {
         ElectricalApplianceFactory factory = new ElectricalApplianceFactory();
         ElectricalAppliance e = factory.create(new String[]{" FRIDGE ","name ", " 100"});
         Assert.assertTrue(e.getPower()==100);
